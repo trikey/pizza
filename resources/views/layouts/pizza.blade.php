881c88c9cdbc7888e4c3a725d5358aeb23af1361
Ivan Belitskii <belitskii@gmail.com>
2020-08-21 00:41:37 +0300

    add styles for currency selector

--- resources/views/layouts/pizza.blade.php
+++ resources/views/layouts/pizza.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/bxslider/4.2.12/jquery.bxslider.css">
-{{--    <link rel="stylesheet" href="{{ asset('css/all.css') }}">--}}
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <script src="{{ asset('js/app.js') }}" type="text/javascript"></script>
@@ -19,12 +18,20 @@
 <div class="header">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-2 logo-web">
+            <div class="col-lg-2 flex-grid logo-web">
                 <div class="logo">
                     <a href="{{ route('index') }}">Pizza</a>
                 </div>
+                <div class="form-row currency">
+                    <div class="my-1">
+                        <select class="form-control form-control-sm mr-sm-2">
+                            <option>$</option>
+                            <option>€</option>
+                        </select>
+                    </div>
+                </div>
             </div>
-            <div class="col-lg-8 col-3 menu-wrapper">
+            <div class="col-lg-8 col-md-4 col-sm-4 col-3 menu-wrapper">
                 <div class="main-menu text-center">
                     <div class="hamburger">
                         <span></span>
@@ -58,7 +65,8 @@
                                         {{ __('Logout') }}
                                     </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
                                         @csrf
                                     </form>
                                 </div>
@@ -68,14 +76,22 @@
                 </div>
             </div>
 
-            <div class="col-lg-2 col-6 logo-mob">
+            <div class="col-lg-2 col-md-4 col-sm-4 col-4 logo-mob">
                 <div class="logo">
                     <a href="{{ route('index') }}">Pizza</a>
                 </div>
             </div>
 
-            <div class="col-lg-2 col-3 text-right">
-                <div class="cart-contact flex-grid">
+            <div class="col-lg-2 col-md-4 col-sm-4 col-5 flex-grid">
+                <div class="form-row currency logo-mob">
+                    <div class="my-1">
+                        <select class="form-control form-control-sm mr-sm-2">
+                            <option>$</option>
+                            <option>€</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="cart-contact flex-grid text-right">
                     <button class="cart-toggle-btn">
                         <a class="flex-grid" href="{{ route('cart.index') }}">
                             <span class="cart-count" id="cart-count"></span>
